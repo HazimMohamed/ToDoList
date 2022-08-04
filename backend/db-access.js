80ff6be13ce4bd048353731674015d65c02ccd4e
root <root@vps.server.local>
2022-08-04 01:44:04 +0000

    init

---
+++ backend/db-access.js
@@ -0,0 +1,16 @@
+function getAllItems() {
+    return {
+	'comments': [
+	    {
+		'createTime': 12314324,
+		'content': "I need to clean the litter box"
+	    },
+	    {
+		'createTime': 12314324,
+		'content': "I should do the dishes"
+	    }
+	]
+    }	
+}
+
+module.exports = { getAllItems }
